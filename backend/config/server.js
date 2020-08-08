222ef1b9109574d947a219d673d718955fda32dd
Jamie Gobeille <djent34@gmail.com>
2020-08-08 14:56:17 +0900

    set up backend of strapi blog. Now setting up frontend

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd837571cb8a7a90b687b96ffe2bbae11'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8c3099bc-ea1e-4b01-a058-5b12c0998029'
+};
